a83ddf187770d1904015f2e1019ecf8d0e0e27a8
Justinio99 <31925156+Justinio99@users.noreply.github.com>
2018-05-30 14:28:51 +0200

    ig machä lämpä

--- app/src/main/java/ch/bbcag/foodwizard/MainActivity.java
+++ app/src/main/java/ch/bbcag/foodwizard/MainActivity.java
@@ -2,12 +2,37 @@ package ch.bbcag.foodwizard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    List search = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ListView schgumpe = findViewById(R.id.schgumpälämpemachetue);
+        Button searchInput = findViewById(R.id.button_send);
+
+        searchInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ArrayAdapter<ingredeats> ingredeatsArray = new ArrayAdapter<>();
+
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/ch/bbcag/foodwizard/SearchPropertys.java
@@ -0,0 +1,19 @@
+package ch.bbcag.foodwizard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.inputmethod.InputBinding;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchPropertys extends AppCompatActivity {
+
+
+
+
+}
+
+
+
